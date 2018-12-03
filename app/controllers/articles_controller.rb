17e025126e2800b5e37df3f0cbedc440c52d3ca3
alexdrottsgard <aalexjd@gmail.com>
2018-12-03 21:46:50 +0100

    create, add, edit articles, show user-feedback

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+include ArticlesHelper
 
     def index
         @articles = Article.all
@@ -7,5 +8,38 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        # fail to see params and what data is extractable from the params-hash
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Article '#{@article.title}' Added!"
+
+        redirect_to article_path(@article)
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id]).destroy
+        flash.notice = "Article '#{@article.title}' Deleted!"
+
+        redirect_to articles_path
+    end
+
 
 end
